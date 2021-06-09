06fba6aa30b165db91cfbbe16f7f19229906e8ef
Alexander <alexmal74@gmail.com>
2021-06-09 11:11:38 +0500

    experimental first version of program

--- main.py
+++ main.py
@@ -7,3 +7,17 @@ from pytube import YouTube
 link = input("Вставьте ссылку на видео Youtube:  ")
 yt = YouTube(link)
 
+# Выводим информацию о видео
+print("Название: ", yt.title)
+print("Автор: ", yt.author)
+print("Просмотры: ", yt.views)
+print("Рейтинг: ", yt.rating)
+print("Продолжительность: ", yt.length, "сек\n")
+
+# Выбираем для загрузки поток с максимальным разрешением
+ys = yt.streams.get_highest_resolution()
+
+# Загрузка
+print("Видео загружается... ")
+ys.download('C:/Users/Uzzer/Downloads')
+print("Загрузка завершена")
